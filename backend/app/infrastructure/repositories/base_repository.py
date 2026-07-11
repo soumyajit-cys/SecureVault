5edfd2163cd7dc2a226a9b6b378496f66f68a9d6
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-11 09:06:08 +0530

    updated base_repository.py

--- backend/app/infrastructure/repositories/base_repository.py
+++ backend/app/infrastructure/repositories/base_repository.py
@@ -2,14 +2,14 @@ from typing import Generic
 from typing import TypeVar
 from uuid import UUID
 
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 T = TypeVar("T")
 
 
-class SQLAlchemyRepository(
-    Generic[T]
-):
+class SQLAlchemyRepository(Generic[T]):
+
     model = None
 
     def __init__(
@@ -22,34 +22,45 @@ class SQLAlchemyRepository(
         self,
         entity_id: UUID,
     ) -> T | None:
-        return (
-            self.db.query(self.model)
-            .filter(
+
+        stmt = (
+            select(self.model)
+            .where(
                 self.model.id == entity_id
             )
-            .first()
         )
+
+        return self.db.scalar(stmt)
 
     def create(
         self,
         entity: T,
     ) -> T:
+
         self.db.add(entity)
-        self.db.commit()
+
+        self.db.flush()
+
         self.db.refresh(entity)
+
         return entity
 
     def update(
         self,
         entity: T,
     ) -> T:
-        self.db.commit()
+
+        self.db.flush()
+
         self.db.refresh(entity)
+
         return entity
 
     def delete(
         self,
         entity: T,
     ) -> None:
+
         self.db.delete(entity)
-        self.db.commit()
+
+        self.db.flush()
