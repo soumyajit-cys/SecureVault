2a28c84313d7e59b4123fee83d44d12a4750a1e5
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-11 09:07:18 +0530

    created unit_of_work.py

---
+++ backend/app/core/unit_of_work.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+
+
+class UnitOfWork:
+
+    def __init__(
+        self,
+        session: Session,
+    ):
+        self.session = session
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
