2c88dddbdfe4bd727b5cc739f8289841ebfd4ad7
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-11 09:01:06 +0530

    created exception.py

---
+++ backend/app/core/exceptions.py
@@ -0,0 +1,34 @@
+class SecureVaultException(Exception):
+    pass
+
+
+class AuthenticationError(SecureVaultException):
+    pass
+
+
+class AuthorizationError(SecureVaultException):
+    pass
+
+
+class InvalidCredentialsError(AuthenticationError):
+    pass
+
+
+class AccountLockedError(AuthenticationError):
+    pass
+
+
+class UserAlreadyExistsError(AuthenticationError):
+    pass
+
+
+class TokenExpiredError(AuthenticationError):
+    pass
+
+
+class InvalidTokenError(AuthenticationError):
+    pass
+
+
+class PermissionDeniedError(AuthorizationError):
+    pass
