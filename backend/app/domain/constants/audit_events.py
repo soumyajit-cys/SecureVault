8237645a96e488cc0ae0ea1b8c072af725ebee97
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-14 08:52:09 +0530

    created audit_events.py

---
+++ backend/app/domain/constants/audit_events.py
@@ -0,0 +1,19 @@
+USER_REGISTERED = "user.registered"
+
+USER_LOGIN = "user.login"
+
+USER_LOGOUT = "user.logout"
+
+PASSWORD_CHANGED = "user.password_changed"
+
+TOKEN_REFRESHED = "token.refreshed"
+
+ACCOUNT_LOCKED = "account.locked"
+
+FILE_ENCRYPTED = "file.encrypted"
+
+FILE_DECRYPTED = "file.decrypted"
+
+FILE_SHARED = "file.shared"
+
+ADMIN_ACTION = "admin.action"
