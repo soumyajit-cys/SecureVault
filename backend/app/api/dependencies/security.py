e6ccbb76bcb565916264983e8cdec95cec96f423
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-16 07:45:40 +0530

    updated security.py

--- backend/app/api/dependencies/security.py
+++ backend/app/api/dependencies/security.py
@@ -2,6 +2,21 @@ from app.services.auth.password_service import (
     Argon2PasswordService,
 )
 
+from app.services.auth.refresh_token_service import (
+    RefreshTokenService,
+)
+
+from app.infrastructure.repositories.refresh_token_repository import (
+    SQLAlchemyRefreshTokenRepository,
+)
 
 def get_password_service():
     return Argon2PasswordService()
+
+def get_refresh_token_service(
+    repository: SQLAlchemyRefreshTokenRepository,
+):
+
+    return RefreshTokenService(
+        repository
+    )
