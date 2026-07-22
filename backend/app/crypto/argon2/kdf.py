d934cc09b1eec89865fade6b142696060f61d434
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-22 07:12:26 +0530

    updated kdf.py

--- backend/app/crypto/argon2/kdf.py
+++ backend/app/crypto/argon2/kdf.py
@@ -1,3 +1,10 @@
+import os
+
+from argon2.low_level import (
+    Type,
+    hash_secret_raw,
+)
+
 from app.crypto.interfaces.kdf import (
     KeyDerivationFunction,
 )
@@ -7,9 +14,54 @@ class Argon2KDF(
     KeyDerivationFunction
 ):
 
+    SALT_SIZE = 16
+
+    KEY_SIZE = 32
+
+    TIME_COST = 3
+
+    MEMORY_COST = 102400
+
+    PARALLELISM = 8
+
+    def generate_salt(
+        self,
+    ) -> bytes:
+
+        return os.urandom(
+            self.SALT_SIZE
+        )
+
     def derive_key(
         self,
         password: str,
         salt: bytes,
     ) -> bytes:
-        raise NotImplementedError
+
+        return hash_secret_raw(
+            secret=password.encode(),
+            salt=salt,
+            time_cost=self.TIME_COST,
+            memory_cost=self.MEMORY_COST,
+            parallelism=self.PARALLELISM,
+            hash_len=self.KEY_SIZE,
+            type=Type.ID,
+        )
+
+    def derive_key_with_new_salt(
+        self,
+        password: str,
+    ) -> tuple[bytes, bytes]:
+
+        salt = (
+            self.generate_salt()
+        )
+
+        key = (
+            self.derive_key(
+                password,
+                salt,
+            )
+        )
+
+        return key, salt
