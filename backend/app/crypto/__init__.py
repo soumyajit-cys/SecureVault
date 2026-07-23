03eeb07fbc35369ae911dbb7d0c1540f40e64d13
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-23 08:31:52 +0530

    updated __init__.py

--- backend/app/crypto/__init__.py
+++ backend/app/crypto/__init__.py
@@ -1,7 +1,15 @@
 from app.crypto.aes.aes_gcm import AESGCMCipher
 from app.crypto.argon2.kdf import Argon2KDF
+from app.crypto.hashing.sha256 import (
+    SHA256Engine,
+)
 
 __all__ = [
     "AESGCMCipher",
     "Argon2KDF",
 ]
+
+__all__ = [
+    "SHA256Engine",
+]
+
