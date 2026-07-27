77bd14aeedcf46260a8c073aac347c348a9b89c9
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-07-27 10:47:51 +0530

    created constants.py

---
+++ backend/app/crypto/streams/constants.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+DEFAULT_CHUNK_SIZE = 4 * 1024 * 1024  # 4 MiB
+
+MIN_CHUNK_SIZE = 64 * 1024            # 64 KiB
+
+MAX_CHUNK_SIZE = 16 * 1024 * 1024     # 16 MiB
