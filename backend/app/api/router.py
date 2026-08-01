66b62dbe13dd194dd13be0811976e87889104141
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-08-02 00:27:02 +0530

    Reorganize router imports and include admin router in API routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,25 +1,35 @@
 from fastapi import APIRouter
 
-from app.api.routes.health import router as health_router
+from app.api.routes.admin import (
+    router as admin_router
+)
 
 from app.api.routes.auth import (
     router as auth_router
+)
+
+from app.api.routes.health import (
+    router as health_router
 )
 
 from app.api.routes.profile import (
     router as profile_router
 )
 
-api_router.include_router(
-    profile_router
-)
-
-
 api_router = APIRouter()
+
+api_router.include_router(
+    admin_router
+)
 
 api_router.include_router(
     auth_router
 )
-api_router.include_router(health_router)
 
+api_router.include_router(
+    health_router
+)
 
+api_router.include_router(
+    profile_router
+)
